a387e13c04684ed6d3bc0223ecdb271392f156b0
Chloe ONeil <chloesporri@yahoo.com>
2022-11-03 15:13:01 -0700

    set const for selection values

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,28 @@ window.addEventListener("load", function() {
   const clickHere= document.getElementById("click here");
   let head= document.getElementById("header");
   let formDiv= document.getElementById("form");
-  let inputName= document.getElementById("input-name");
   let favPet= document.getElementById("pet-type");
   let favColor= document.getElementById("color-type");
   let favHoliday= document.getElementById("holiday-type");
   let favMusic= document.getElementById("music-type");
   let favMovie= document.getElementById("movie-type");
   let nameSpan= document.getElementById("name-span");
-  
-}
 
-function handleSelect(event) {
-  event.preventDefault();
-  const selection=documentGetElementById("pet-type").value;
+  function handleClickHere () {
+    head.classList.add("invisible");
+    formDiv.classList.remove("invisible");
+  }
+
+  function handleSelect(event) {
+    event.preventDefault();
+    const name= document.getElementById("input-name").value;
+    const select1=document.getElementById("pet-type").value;
+    const select2=document.getElementById("color-type").value;
+    const select3=document.getElementById("holiday-type").value;
+    const select4=document.getElementById("music-type").value;
+    const select5=document.getElementById("movie-type").value;
+
+
 }
 window.addEventListener("load", function() {
   document.getElementById ("select-form").addEventListener("submit", handleSelect);
