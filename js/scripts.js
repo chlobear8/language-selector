70c56a3b01485c805ade48802f5ff2fa6c5dd3a6
Chloe ONeil <chloesporri@yahoo.com>
2022-11-03 14:55:14 -0700

    add let commands under eventListener

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
+window.addEventListener("load", function() {
+  const clickHere= document.getElementById("click here");
+  let head= document.getElementById("header");
+  let formDiv= document.getElementById("form");
+  let inputName= document.getElementById("input-name");
+  let favPet= document.getElementById("pet-type");
+  let favColor= document.getElementById("color-type");
+  let favHoliday= document.getElementById("holiday-type");
+  let favMusic= document.getElementById("music-type");
+  let favMovie= document.getElementById("movie-type");
+  let nameSpan= document.getElementById("name-span");
+  
+}
+
 function handleSelect(event) {
   event.preventDefault();
   const selection=documentGetElementById("pet-type").value;
 }
-window.addEventListener("load", function()) {
+window.addEventListener("load", function() {
   document.getElementById ("select-form").addEventListener("submit", handleSelect);
 });
